b343b45998cbadc87469788aff7cb10592fc8a8c
sbarsan <sorinbarsan@gmail.com>
2022-05-07 18:26:43 +0300

    Add project files.

---
+++ LongPollingServer/Controllers/LongPollingController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LongPollingServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LongPollingController : ControllerBase
+    {
+        private readonly ILogger<LongPollingController> _logger;
+
+        public LongPollingController(ILogger<LongPollingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ExecuteLongPolling([FromQuery, Required] string orderNumber)
+        {
+            var longPolling = new LongPolling(orderNumber);
+            var orderStatus = await longPolling.GetStatusAsync();
+            return new ObjectResult(new { OrderStatus = (orderStatus != null ? orderStatus : "Long polling timeout!") });
+        }
+
+        [HttpPost]
+        public void SimulateStateChange([FromQuery, Required] string orderNumber, string status)
+        {
+            LongPolling.SetStatus(orderNumber, status);
+        }
+    }
+}
---
+++ LongPollingServer/LongPolling.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LongPollingServer
+{
+    public class LongPolling
+    {
+        const int Timeout = 20000; // TO DO: let client set this value!
+
+        private static List<LongPolling> s_Subscribers = new List<LongPolling>();
+
+        private TaskCompletionSource<bool> _taskCompletion = new TaskCompletionSource<bool>();
+
+        private string OrderNumber { get; set; }
+        private string Status { get; set; }
+
+        public LongPolling(string orderNumber)
+        {
+            OrderNumber = orderNumber;
+            lock (s_Subscribers)
+            {
+                s_Subscribers.Add(this);
+            }
+        }
+
+        private void Notify(string status)
+        {
+            Status = status;
+            _taskCompletion.SetResult(true);
+        }
+
+        public async Task<string> GetStatusAsync()
+        {
+            await Task.WhenAny(_taskCompletion.Task, Task.Delay(Timeout));
+            lock (s_Subscribers)
+            {
+                s_Subscribers.Remove(this);
+            }
+            return Status;
+        }
+
+        public static void SetStatus(string orderNumber, string status)
+        {
+            lock (s_Subscribers)
+            {
+                var subscriber = s_Subscribers.ToList();
+                foreach (var poll in subscriber)
+                {
+                    if (poll.OrderNumber == orderNumber)
+                        poll.Notify(status);
+                }
+            }
+        }
+    }
+}
